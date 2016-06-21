c593ad58469d8c9065098e45cb47edb405bb491e
tahirhsn91 <tahirhsn91@gmail.com>
2016-06-21 06:56:56 +0500

    initial structure

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by tahirhsn on 6/21/2016.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
